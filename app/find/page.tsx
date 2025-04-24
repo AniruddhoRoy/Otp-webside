7769f74a3b484f3c9035a706a9af28a573b00eda
Your Name <mytestinggitmain@aniruddhoroy.com>
2025-04-24 23:38:54 +0600

    done 2

--- app/find/page.tsx
+++ app/find/page.tsx
@@ -5,14 +5,13 @@ import {findObjSchema} from "../../types/zod"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import { find_obj } from "@/actions/find_obj";
 import { useState } from "react";
-import { create_obj } from "@/actions/create_obj";
 import { toast } from "sonner"
 import { send_otp } from "@/actions/send_otp";
 import { verify_otp } from "@/actions/verify_otp";
 import Link from "next/link";
 import { AlertDialogBox } from "@/components/custom/alertDialog";
+import { SyncLoader } from "react-spinners";
 
 export default function Home() {
 
@@ -27,10 +26,12 @@ export default function Home() {
     const [success,setSuccess] = useState("")
     const [secretText,setsecretText] = useState("")
     const [showOtpBox,setShowOtpBox] = useState(false)
+    const [isLoading,setIsloading] = useState(false)
     const onSubmutHandler:SubmitHandler<z.infer< typeof findObjSchema>> = (values)=>{
         
       setError("");
       setSuccess("");
+      setIsloading(true);
       if(!values.otp){
         send_otp(values.id).then((data)=>{
           if(data){
@@ -39,19 +40,25 @@ export default function Home() {
             setShowOtpBox(true);
             return;
           }
+        }).finally(()=>{
+          setIsloading(false)
         })
       }else{
         if(values.otp?.length!=6){
           setError("Invalid Otp")
+          setIsloading(false)
           return;
         }
         verify_otp(values.id,values.otp).then((data)=>{
           if(data){
             setsecretText(data.text);
+            setShowOtpBox(false);
           }else{
             setError("Invalid Otp")
             return;
           }
+        }).finally(()=>{
+          setIsloading(false)
         })
       }
       
@@ -62,19 +69,29 @@ export default function Home() {
     <div className="h-full w-full flex justify-center items-center px-2">
       <form onSubmit={handleSubmit(onSubmutHandler)} className="w-full max-w-[400px] space-y-2  shadow-xl px-3 py-4 rounded-xl bg-zinc-50">
         <h3>ID:</h3>
-        <Input {...register("id")}/>
+        <Input {...register("id")} disabled={isLoading || showOtpBox}/>
         {errors.id && <p className="text-red-500 font-semibold">{errors.id.message}</p>}
         {showOtpBox && <>
         <h3>OTP:</h3>
-        <Input {...register("otp")}/>
+        <Input {...register("otp")} disabled={isLoading}/>
         {errors.otp && <p className="text-red-500 font-semibold">{errors.otp.message}</p>}
         </>}
         <div className="w-full text-center pt-2">
-        <Button size={"lg"} type="submit" >Submit</Button>
-        <h1>Or</h1>
+        {isLoading ? <SyncLoader color="oklch(55.4% 0.046 257.417)"/> :<>{!secretText && <Button size={"lg"} >Submit</Button> }</>}
+        {!secretText && <h1>Or</h1>}
         <Link href={"/"}>
       <Button variant={"link"} type="button" className="text-blue-600 cursor-pointer">Create New encrypted text</Button>
         </Link>
+        {secretText && 
+        <Button 
+          variant={"link"} 
+          type="reset" 
+          className="text-blue-600 cursor-pointer"
+          onClick={() => window.location.reload()}
+        >
+          View another encrypted text
+        </Button>
+      }
         </div>
         {secretText && <>
         <div className="w-full text-center pt-2">
@@ -82,7 +99,7 @@ export default function Home() {
         </div>
         </>}
         {error && <p className="text-red-500 font-semibold">{error}</p>}
-        {success && <p className="text-green-500 font-semibold">{success}</p>}
+        {success && <p className="text-gray-500 font-semibold">{success}</p>}
       </form>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { create_obj } from "@/actions/create_obj";
 import { toast } from "sonner"
 import Link from "next/link";
+import {SyncLoader} from "react-spinners" 
 
 export default function Home() {
 
@@ -22,9 +23,11 @@ export default function Home() {
       resolver:zodResolver(createObjSchema)
     });
     const [error,setError] = useState("")
+    const [isLoading,setIsloading] = useState(false)
     const onSubmutHandler:SubmitHandler<z.infer< typeof createObjSchema>> = (values)=>{
       // console.log(values)
       setError("");
+      setIsloading(true);
       find_obj(values.id).then((data)=>{
         if(data){
           setError("😒 Id Not Available")
@@ -40,25 +43,30 @@ export default function Home() {
             // console.log("Successfull");
             reset();
         })
+      }).finally(()=>{
+        setIsloading(false)
       })
     }
   return (
     <div className="h-full w-full flex flex-col justify-center items-center px-2">
       <form onSubmit={handleSubmit(onSubmutHandler)} className="w-full max-w-[400px] space-y-2  shadow-xl px-3 py-4 rounded-xl bg-zinc-50">
         <h3>ID:</h3>
-        <Input {...register("id")}/>
+        <Input {...register("id")} disabled={isLoading}/>
         {errors.id && <p className="text-red-500 font-semibold">{errors.id.message}</p>}
         <h3>Email:</h3>
-        <Input {...register("email")}/>
+        <Input {...register("email")} disabled={isLoading}/>
         {errors.email && <p className="text-red-500 font-semibold">{errors.email.message}</p>}
         <h3>Secret Text:</h3>
-        <Input {...register("secretText")}/>
+        <Input {...register("secretText")} disabled={isLoading}/>
         {errors.secretText && <p className="text-red-500 font-semibold">{errors.secretText.message}</p>}
         <div className="w-full text-center pt-2">
-        <Button size={"lg"} >Submit</Button>
+          {isLoading ? <SyncLoader color="oklch(55.4% 0.046 257.417)"/> :<Button size={"lg"} >Submit</Button> }
+        
+        
         <h1>Or</h1>
-        <Link href={"/find"}>
-      <Button variant={"link"} type="button" className="text-blue-600 cursor-pointer">Decode existing text</Button>
+        
+        <Link href={"/find"} >
+      <Button variant={"link"} type="button" className="text-blue-600 cursor-pointer" >Decode existing text</Button>
         </Link>
         </div>
         {error && <p className="text-red-500 font-semibold">{error}</p>}
